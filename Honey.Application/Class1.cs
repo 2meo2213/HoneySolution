97a7b7e33914d3f4ee12a3332551297384369823
lego9x <lego9x@gmail.com>
2020-11-02 23:17:05 +0700

    create struct

---
+++ Honey.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Honey.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ Honey.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Honey.Data
+{
+    public class Class1
+    {
+    }
+}
