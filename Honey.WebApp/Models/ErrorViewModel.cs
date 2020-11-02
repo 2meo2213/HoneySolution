7236770eda275fbdbe3178f2c18a2b89f64d71e9
lego9x <lego9x@gmail.com>
2020-11-02 22:47:07 +0700

    Add project files.

---
+++ Honey.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Honey.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
